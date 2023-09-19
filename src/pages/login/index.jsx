bc4171f2f8a895b535e4541340675599dd3d0ce6
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-09-19 10:11:57 +0700

    feat(login): slicing login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,64 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
-export default Login
+import Layout from "../../components/Layout";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: `Welcome, ${username}`,
+      confirmButtonText: "OK",
+    }).then((res) => {
+      if (res.isConfirmed) {
+        navigate("/");
+      }
+    });
+  };
+
+  return (
+    <Layout>
+      <form
+        onSubmit={handleLogin}
+        style={{
+          width: 250,
+          height: 200,
+          display: "flex",
+          flexDirection: "column",
+          rowGap: 35,
+        }}
+      >
+        <Input
+          id="username"
+          name="username"
+          label="Username"
+          type="text"
+          placeholder="type your username here ..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="type your password here ..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button id="login" type="submit" label="Login" />
+      </form>
+    </Layout>
+  );
+};
+
+export default Login;
